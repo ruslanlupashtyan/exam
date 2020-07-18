114304002ea1f72a6bb325bcc65699626c5de5cc
Пользователь <ruslanl1488@gmail.com>
2020-07-19 00:46:04 +0300

    12

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggeMenu() {
+    let menu = document.getElementById("main-menu");
+    menu.classList.toggle("menu-active");
+
+    let button = document.getElementById("hamburger-1");
+    button.classList.toggle("is-active");
+}
